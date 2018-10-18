370b229eca0e8ec180e33c8f715d44e286bcf23f
Dreaddisk <joey_manuel@outlook.com>
2018-10-18 13:13:48 -0400

    3. Rocket
    The rocket object and script have been setup.

---
+++ Rocket_Booster_Prototype/Assets/Scripts/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Going to the left");
+        }
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Going to the right");
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Thrusting");
+        }
+    }
+} // main class
