4b8fe2a54a9b557a0fbb6376411b56ff577844c3
Dreaddisk <joey_manuel@outlook.com>
2018-10-21 11:29:52 -0400

    4. Rocket 2
    Directional and propelling are in oder (needs tweaking).
    Audio for the propelling is set.

--- Rocket_Booster_Prototype/Assets/Scripts/Rocket.cs
+++ Rocket_Booster_Prototype/Assets/Scripts/Rocket.cs
@@ -5,29 +5,53 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     #region Variables
+    private Rigidbody rigidBody;
+    AudioSource audioSource;
 
     #endregion
 
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
     private void Update()
     {
         ProcessInput();
+
+
     }
 
     private void ProcessInput()
     {
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            //            rigidBody.AddRelativeForce(Vector3.left);
+            //           rigidBody.AddRelativeTorque(Vector3.up);
+            transform.Rotate(Vector3.forward);
             Debug.Log("Going to the left");
         }
 
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            //            rigidBody.AddRelativeForce(Vector3.right);
+            //            rigidBody.AddRelativeTorque(Vector3.down);
+            transform.Rotate(Vector3.back);
             Debug.Log("Going to the right");
         }
 
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Stop();
         }
     }
+
 } // main class
