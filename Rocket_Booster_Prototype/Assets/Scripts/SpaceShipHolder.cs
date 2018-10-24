ba2620137630fe545c6f0e72e5ce30467134441f
Dreaddisk <joey_manuel@outlook.com>
2018-10-24 17:19:46 -0400

    8. Space Holder
    Created a Space rocket holder that gets activated after the rocket reach the landing pad.

---
+++ Rocket_Booster_Prototype/Assets/Scripts/SpaceShipHolder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipHolder : MonoBehaviour
+{
+    #region Variables
+    public GameObject rocketHolder;
+    #endregion
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            rocketHolder.SetActive(true);
+        }
+    }
+
+}// main class
